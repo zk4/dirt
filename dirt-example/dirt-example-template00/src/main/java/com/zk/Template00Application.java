3d09cef665e0b693739d7279e0a473f7d81747d2
zk <liuzq7@gmail.com>
2022-09-20 11:23:27 +0800

    chore: remove unused annotation

--- dirt-example/dirt-example-template00/src/main/java/com/zk/Template00Application.java
+++ dirt-example/dirt-example-template00/src/main/java/com/zk/Template00Application.java
@@ -2,7 +2,6 @@ package com.zk;
 
 
 import com.zk.dirt.util.SpringUtil;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,7 +12,6 @@ import static com.zk.utils.StartUtils.logApplicationStartup;
 
 
 @SpringBootApplication
-@Slf4j
 @EnableAsync
 public class Template00Application {
     public static void main(String[] args) {
