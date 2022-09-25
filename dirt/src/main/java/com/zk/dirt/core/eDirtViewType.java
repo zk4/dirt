8f9097d9ef28537dff7567f603f352dfc1f38de1
zk <liuzq7@gmail.com>
2022-09-25 13:20:38 +0800

    chore: add more view type

--- dirt/src/main/java/com/zk/dirt/core/eDirtViewType.java
+++ dirt/src/main/java/com/zk/dirt/core/eDirtViewType.java
@@ -2,8 +2,25 @@ package com.zk.dirt.core;
 
 public enum eDirtViewType  {
     // 表格
+    // [----]
+    // [----]
     Table,
 
+    // [-][----]
+    // [-][----]
     //左树右表格
     Tree,
+
+    //时间轴
+    // *[-]
+    // *[-]
+    // *[-]
+    TimeLine,
+
+
+    // 卡片墙
+    // [][][]
+    // [][][]
+    CardWall
+
 }
