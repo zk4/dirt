4873d9bb34faf72897709b9edb3730016c6ee237
zk <liuzq7@gmail.com>
2022-10-20 13:04:06 +0800

    wip: delete ok.but child data not showing

--- dirt-example/dirt-example-experiment/src/main/java/com/zk/experiment/Documentation.java
+++ dirt-example/dirt-example-experiment/src/main/java/com/zk/experiment/Documentation.java
@@ -6,21 +6,15 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Getter
 @Setter
 @Entity
 @DynamicUpdate
 @DynamicInsert
-@Table(name = "documentation")
-@SQLDelete(sql = "UPDATE documentation SET deleted = true WHERE id=?  and version=? ")
-@Where(clause = "deleted=false")
 @JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"})
 @DiscriminatorValue("documentation")
 public class Documentation extends WindowFile {
--- dirt-example/dirt-example-experiment/src/main/java/com/zk/experiment/Folder.java
+++ dirt-example/dirt-example-experiment/src/main/java/com/zk/experiment/Folder.java
@@ -1,25 +1,20 @@
 package com.zk.experiment;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.zk.dirt.annotation.DirtEntity;
 import com.zk.dirt.annotation.DirtField;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 
-import javax.persistence.*;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 
 @Getter
 @Setter
 @Entity
 @DynamicUpdate
 @DynamicInsert
-@Table(name = "folder")
-@SQLDelete(sql = "UPDATE folder SET deleted = true WHERE id=?  and version=? ")
-@Where(clause = "deleted=false")
 @JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"})
 @DiscriminatorValue("Folder")
 public class Folder extends WindowFile {
