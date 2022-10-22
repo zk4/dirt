bbfef00165228d06bfbd6ee72d0fd19a7fe112bb
zk <liuzq7@gmail.com>
2022-10-23 03:55:39 +0800

    feat: using location search  for history

--- dirt-ui/src/App.js
+++ dirt-ui/src/App.js
@@ -1,12 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect,useSearchParams} from "react";
 import 'antd/dist/antd.css';
 import axios from 'axios'
 import Table from './components/dirtTable'
+import qs from 'qs'
 import Tree from './components/tree'
 
 function App() {
   let [tables, setTables] = useState({})
-  const pathname= window.location.pathname.substr(1);
+  let entityName =qs.parse(window.location.search, { ignoreQueryPrefix: true }).entityName
+  entityName=entityName 
+
   const [title, setTitle] = useState(null);
   const [viewType, setViewType] = useState('Table');
   
@@ -17,7 +20,8 @@ function App() {
     (async () => {
       let data = await axios.get(`getTableMaps`)
       setTables(data.data);
-      setTitle(data.data[pathname].text)
+      if(entityName!=null)
+        setTitle(data.data[entityName].text)
     })()
   }, [])
   return (
@@ -25,14 +29,16 @@ function App() {
       {
         Object.keys(tables).length > 0 && Object.entries(tables).map(([t, v]) => {
           return <a key={t} href="#!" onClick={e => {
-            window.location.pathname = v.className;
+            // window.location.entityName = v.className;
+          window.location.search = "entityName="+v.className;
+
           }}>{v.text} |</a>
         })
       }
       <h1>{title}</h1>
       <hr style={{marginBottom: '25px'}} />
-      {viewType === 'Table' && (< Table entityName={pathname} />)}
-      {viewType === 'Tree' && (< Table entityName={pathname} />)}
+      {viewType === 'Table' && (< Table entityName={entityName} />)}
+      {viewType === 'Tree' && (< Table entityName={entityName} />)}
     </div>
   );
 }
