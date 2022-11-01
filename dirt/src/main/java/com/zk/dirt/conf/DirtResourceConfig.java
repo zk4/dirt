38890efec7cba5667ec71742015b90cea456ab72
zk <liuzq7@gmail.com>
2022-11-01 18:13:22 +0800

    chore: update to 0.0.6 beta

--- dirt/src/main/java/com/zk/dirt/conf/DirtResourceConfig.java
+++ dirt/src/main/java/com/zk/dirt/conf/DirtResourceConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class DirtResourceConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/dirt/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/dirt/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/dirt/**").addResourceLocations("classpath:/static/");
     }
 }
