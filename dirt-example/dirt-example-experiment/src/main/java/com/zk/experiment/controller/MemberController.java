e012031a326ec9e3fda9c2bed56111df311b65f8
zk <liuzq7@gmail.com>
2022-11-08 17:57:07 +0800

    feat: demo for DSL

--- dirt-example/dirt-example-experiment/src/main/java/com/zk/experiment/controller/MemberController.java
+++ dirt-example/dirt-example-experiment/src/main/java/com/zk/experiment/controller/MemberController.java
@@ -1,11 +1,18 @@
 package com.zk.experiment.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zk.dirt.conf.DirtQueryFilter;
 import com.zk.dirt.service.DirtService;
+import com.zk.dirt.wrapper.Result;
 import com.zk.experiment.entity.Member;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-public class CreateMember {
+@Api(tags = {"会员"})
+public class MemberController {
     @Autowired
     DirtService dirtService;
 
@@ -21,6 +29,7 @@ public class CreateMember {
     ObjectMapper objectMapper;
 
     @PostMapping("/mmeber/create")
+    @ApiOperation(value = "创建会员")
     @SneakyThrows
     public void createMember(@RequestBody  CreateMemberReq req)  {
         Map map = objectMapper.convertValue(req, Map.class);
@@ -31,5 +40,15 @@ public class CreateMember {
     public static  class CreateMemberReq {
         String name;
     }
+
+    @PostMapping(value = "/mmeber/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "搜索会员")
+    @SneakyThrows
+    public String searchMember(@RequestBody DirtQueryFilter reqFilter,  Pageable pageable)  {
+        Page<Object> page = dirtService.page(reqFilter, Member.class.getName(), pageable);
+        return objectMapper.writeValueAsString(Result.success(page));
+    }
+
+
 
 }
