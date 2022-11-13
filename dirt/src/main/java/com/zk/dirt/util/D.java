6f8f0277ddf78fc348bafd0ac833634e243ddfc9
zk <liuzq7@gmail.com>
2022-11-14 03:46:52 +0800

    chore: update

--- dirt/src/main/java/com/zk/dirt/util/D.java
+++
@@ -1,8 +0,0 @@
-package com.zk.dirt.util;
-
-public class D {
-    public static <T> T getBean(Class<T> clazz) {
-        return SpringUtil.getBean(clazz);
-    }
-
-}
--- dirt/src/main/java/com/zk/dirt/util/ExceptionUtils.java
+++
@@ -1,9 +0,0 @@
-package com.zk.dirt.util;
-
-public class ExceptionUtils {
-    public static void conditionThrow(boolean b,String msg ){
-        if(!b){
-            throw new RuntimeException(msg);
-        }
-    }
-}
