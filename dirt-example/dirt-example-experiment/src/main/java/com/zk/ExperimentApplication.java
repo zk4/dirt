589f97fb2861c088a75feab5811d5217b218f683
zk <liuzq7@gmail.com>
2023-02-14 01:52:30 +0800

    chore: u

--- dirt-example/dirt-example-experiment/src/main/java/com/zk/ExperimentApplication.java
+++ dirt-example/dirt-example-experiment/src/main/java/com/zk/ExperimentApplication.java
@@ -27,7 +27,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @Slf4j
 @EnableAsync
-@DirtScan({"com.zk.images"})
+@DirtScan({"com.zk.experiment"})
 //@EntityScan({"com.zk.dirt.entity","com.zk.relations","com.zk.experiment.jpa"})
 
 public class ExperimentApplication {
